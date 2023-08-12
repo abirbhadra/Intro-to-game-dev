f991c3ab10ff5749516ef8f9731cd3d8949aaf37
abirbhadra <58373108+abirbhadra@users.noreply.github.com>
2023-08-13 04:08:02 +0530

    Main Menu
    Main Menu Page set with three buttons and each button directing to a different scenes and those 3 scenes have back button redirecting to the main menu scene.

---
+++ Intro to game dev/Assets/Scripts/BackButtonScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class BackButtonScript : MonoBehaviour
+{
+    public Button button;
+    public string NewScene;
+    // Start is called before the first frame update
+    private void Start()
+    {
+        button.onClick.AddListener(OnButtonClick);
+    }
+    private void OnButtonClick()
+    {
+        Debug.Log("Back Button Clicked");
+        SceneManager.LoadScene(NewScene);
+    }
+
+}
---
+++ Intro to game dev/Assets/Scripts/MyFirstScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class MyFirstScript : MonoBehaviour
+{
+    public Button button1;
+    public Button button2;
+    public Button button3;
+    public string NewScene1;
+    public string NewScene2;
+    public string NewScene3;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        button1.onClick.AddListener(OnButtonClick1);
+        button2.onClick.AddListener(OnButtonClick2);
+        button3.onClick.AddListener(OnButtonClick3);
+    }
+    private void OnButtonClick1()
+    {
+        Debug.Log("Button 1 Clicked");
+        SceneManager.LoadScene(NewScene1);
+    }
+    private void OnButtonClick2()
+    {
+        Debug.Log("Button 1 Clicked");
+        SceneManager.LoadScene(NewScene2);
+    }
+    private void OnButtonClick3()
+    {
+        Debug.Log("Button 1 Clicked");
+        SceneManager.LoadScene(NewScene3);
+    }
+
+}
